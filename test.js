ad56a49a0f47ac7847f0639e8d3e362e244fd25e
Kanat Asanbekov <kanatqa@gmail.com>
2019-12-26 23:42:29 -0800

    added new first test js file

---
+++ test.js
@@ -0,0 +1,15 @@
+howMuchILoveYou(7);
+function howMuchILoveYou(nbPetals) {
+    // your code
+    let petals = ['I love you',
+        'a little',
+        'a lot',
+        'passionately',
+        'madly',
+        'not at all'];
+    for(let i = 0; i < nbPetals; i++){
+        if(nbPetals[i] == i){
+            console.log(nbPetals[i]);
+        }
+    }
+  }
